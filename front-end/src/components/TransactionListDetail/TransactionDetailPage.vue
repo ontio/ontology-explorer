d9dc013163b278828629dab166324c496222a68b
Link Su <sulingxiao@onchain.com>
2018-05-07 11:39:17 +0800

    add mobile version

--- front-end/src/components/TransactionListDetail/TransactionDetailPage.vue
+++ front-end/src/components/TransactionListDetail/TransactionDetailPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container container-padding container-margin-top">
-    <div class="div-transaction-detail-page form-group">
+    <div class="div-transaction-detail-page form-group pc-display">
       <div class="row">
         <div class="col-lg-6">
           <p  class=" font-Regular normal_color font-size18 title-more float-left block-detail-page-check-hand" @click="toReturn"><< {{ $t('all.return') }}</p>
@@ -96,7 +96,94 @@
               <div class="col-lg-1 ">sent</div>
               <div class="col-lg-2 ">{{tx.Amount}} {{transactionDetail.info.Detail.AssetName}}</div>
               <div class="col-lg-1 ">to</div>
-              <div class="col-lg-4 padding0-left pointer " @click="toAddressDetailPage(tx.FromAddress)">{{tx.ToAddress}}</div>
+              <div class="col-lg-4 padding0-left pointer " @click="toAddressDetailPage(tx.ToAddress)">{{tx.ToAddress}}</div>
+            </div>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+<!-- mobile -->
+    <div class="div-transaction-detail-page form-group mobile-display">
+      <div class="row">
+        <div class="col-lg-12">
+          <p  class="text-center font-size28 font-ExtraLight p_margin_bottom_L normal_color" >TRANSACTION DETAILS</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <p class="wordbreak font-size18 color32a4be font-blod important_color">Transaction Hash: <span style="font-size:14px;">{{  transactionDetail.info.TxnHash }}</span></p>
+        </div>
+      </div>
+      <table class="table table-hover">
+        <thead>
+        <tr>
+        </tr>
+        </thead>
+        <tbody>
+        <tr >
+          <td class="td11 table1_item_title td_height3 font-size16 normal_color">
+            Transaction Time: {{getDate(transactionDetail.info.TxnTime)}}
+          </td>
+        </tr>
+        <tr >
+          <td class="td11 table1_item_title td_height3 font-size16 normal_color" v-if="transactionDetail.info.TxnType != 209">
+            Type: Deploy Smart Contract
+          </td>
+          <td class="td11 table1_item_title td_height3 font-size16 normal_color" v-else>
+            Type: Smart Contract
+          </td>
+        </tr>
+        <tr >
+          <td class="td11 table1_item_title td_height3 font-size16 normal_color" >
+            Block Height: <span class=" important_color">{{transactionDetail.info.Height}}</span>
+          </td>
+        </tr>
+        <tr >
+          <td class="td11 table1_item_title td_height3 font-size16 normal_color" >
+            Fee: {{transactionDetail.info.Fee}}
+          </td>
+        </tr>
+        <tr >
+          <td class="td11 table1_item_title td_height3 font-size16 normal_color"  v-if="transactionDetail.info.ConfirmFlag == 1">
+            Status: <span style="color:#00AE1D">Confirmed</span>
+          </td>
+          <td class="td11 table1_item_title td_height3 font-size16 normal_color" v-else>
+            Status: <span style="color:#AFACAC" >Failed</span>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+      <table v-if="idflag" class="table table-hover">
+        <thead>
+        <tr>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td class="td11 td_height" style="padding: 34px 24px;">
+            <p class="font-size24  p_margin_bottom f_color font-Regular">OntId:</p>
+            <p class="font-size14 important_color p_margin_bottom font-Regular pointer click_able" >{{Detail.OntId}}</p>
+            <p class="font-size24  p_margin_bottom f_color font-Regular">Description:</p>
+            <p class="font-size14 f_color p_margin_bottom font-Regular">{{Detail.Description}}</p>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+      <table v-if="txflag" class="table table-hover">
+        <thead>
+        <tr>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="tx in Detail.TransferList">
+          <td class="td11 td_height" style="background-color:#32A4BE;color:white;padding: 34px 24px;">
+            <div class="row ">
+              <div class="col-lg-4 padding0-right pointer">{{tx.FromAddress}}</div>
+              <div class="col-lg-1 ">sent</div>
+              <div class="col-lg-2 ">{{tx.Amount}} {{transactionDetail.info.Detail.AssetName}}</div>
+              <div class="col-lg-1 ">to</div>
+              <div class="col-lg-4 padding0-left pointer " >{{tx.ToAddress}}</div>
             </div>
           </td>
         </tr>
