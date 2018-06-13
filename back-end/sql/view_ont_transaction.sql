3f4ac58e5d0d5c03e6f10e273c4549283b73fad2
zzsZhou <zhouqiang@onchain.com>
2018-06-13 20:36:15 +0800

    修改视图生成逻辑

--- back-end/sql/view_ont_transaction.sql
+++ back-end/sql/view_ont_transaction.sql
@@ -3,16 +3,21 @@
 -- ----------------------------
 drop view if exists view_ont_transaction;
 
+
 CREATE VIEW view_ont_transaction AS SELECT DISTINCT
-	(a.txnhash) AS txnhash,
-	a.height AS height,
-	a.txntype AS txntype,
-	a.txntime AS txntime,
-	a.description AS description,
-	a.blockindex AS blockindex,
-	a.confirmflag AS confirmflag,
-	a.fee AS fee
+	`a`.`txnhash` AS `txnhash`,
+	`a`.`height` AS `height`,
+	`a`.`txntype` AS `txntype`,
+	`a`.`txntime` AS `txntime`,
+	`a`.`description` AS `description`,
+	`a`.`blockindex` AS `blockindex`,
+	`a`.`confirmflag` AS `confirmflag`,
+	`a`.`fee` AS `fee`
 FROM
-	tbl_ont_txn_detail a;
+	`tbl_ont_txn_detail` `a`
+WHERE
+	(
+		`a`.`description` <> 'gasconsume'
+	)
 	
 	
