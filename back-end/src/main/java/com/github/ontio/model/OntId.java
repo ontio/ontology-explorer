83fafc3cca503925401b6d89b5c814e368bff9cf
zzsZhou <zhouqiang@onchain.com>
2018-06-27 11:59:21 +0800

    update fee手续费类型

--- back-end/src/main/java/com/github/ontio/model/OntId.java
+++ back-end/src/main/java/com/github/ontio/model/OntId.java
@@ -1,23 +1,6 @@
-/*
- * Copyright (C) 2018 The ontology Authors
- * This file is part of The ontology library.
- *
- * The ontology is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * The ontology is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-
 package com.github.ontio.model;
+
+import java.math.BigDecimal;
 
 public class OntId {
     private String txnhash;
@@ -32,7 +15,7 @@ public class OntId {
 
     private String description;
 
-    private Long fee;
+    private BigDecimal fee;
 
     public String getTxnhash() {
         return txnhash;
@@ -82,11 +65,11 @@ public class OntId {
         this.description = description == null ? null : description.trim();
     }
 
-    public Long getFee() {
+    public BigDecimal getFee() {
         return fee;
     }
 
-    public void setFee(Long fee) {
+    public void setFee(BigDecimal fee) {
         this.fee = fee;
     }
 }
