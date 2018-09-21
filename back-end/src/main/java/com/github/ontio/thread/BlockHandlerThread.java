d8b2b607c802573c93f4d7413537e5f2a98d123e
zzsZhou <zhouqiang@onchain.com>
2018-09-21 16:58:51 +0800

    query before delete

--- back-end/src/main/java/com/github/ontio/thread/BlockHandlerThread.java
+++ back-end/src/main/java/com/github/ontio/thread/BlockHandlerThread.java
@@ -25,6 +25,7 @@ import com.github.ontio.asyncService.BlockHandleService;
 import com.github.ontio.dao.CurrentMapper;
 import com.github.ontio.dao.OntIdMapper;
 import com.github.ontio.dao.TransactionDetailMapper;
+import com.github.ontio.network.exception.ConnectorException;
 import com.github.ontio.utils.ConfigParam;
 import com.github.ontio.utils.ConstantParam;
 import org.slf4j.Logger;
@@ -33,6 +34,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 /**
  * @author zhouq
@@ -43,7 +46,7 @@ import org.springframework.stereotype.Component;
 @Scope("prototype")
 public class BlockHandlerThread extends Thread {
 
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private static final Logger logger = LoggerFactory.getLogger(BlockHandlerThread.class);
 
     private final String CLASS_NAME = this.getClass().getSimpleName();
 
@@ -63,18 +66,17 @@ public class BlockHandlerThread extends Thread {
 
     @Override
     public void run() {
+
         logger.info("========{}.run=======", CLASS_NAME);
         try {
-            ConstantParam.NODE_RETRYMAXTIME = configParam.NODE_AMOUNT * configParam.NODE_INTERRUPTTIME_MAX;
-            ConstantParam.MASTERNODE_RESTFULURL = configParam.MASTERNODE_RESTFUL_URL;
 
+            ConstantParam.MASTERNODE_RESTFULURL = configParam.MASTERNODE_RESTFUL_URL;
             //初始化node列表
             initNodeRestfulList();
             //初始化sdk object
             initSdkService();
 
             int oneBlockTryTime = 1;
-
             while (true) {
 
                 int remoteBlockHieght = getRemoteBlockHeight();
@@ -104,17 +106,17 @@ public class BlockHandlerThread extends Thread {
 
                 //每次删除当前current表height+1的交易，防止上次程序异常退出时，因为多线程事务插入了height+1的交易而current表height未更新
                 //本次同步再次插入会报主键重复异常
-                ontIdMapper.deleteByHeight(dbBlockHeight + 1);
-                transactionDetailMapper.deleteByHeight(dbBlockHeight + 1);
+                if(ontIdMapper.selectCountByHeight(dbBlockHeight + 1) != 0) {
+                    ontIdMapper.deleteByHeight(dbBlockHeight + 1);
+                }
+                if(transactionDetailMapper.selectCountByHeight(dbBlockHeight + 1) != 0) {
+                    transactionDetailMapper.deleteByHeight(dbBlockHeight + 1);
+                }
 
                 //handle blocks and transactions
                 for (int tHeight = dbBlockHeight + 1; tHeight <= remoteBlockHieght; tHeight++) {
                     JSONObject blockJson = getBlockJsonByHeight(tHeight);
-                    String blockBookKeeper = "";
-                    if (tHeight >= 1) {
-                        blockBookKeeper = getBlockBookKeeperByHeight(tHeight - 1);
-                    }
-                    blockManagementService.handleOneBlock(blockJson, blockBookKeeper);
+                    blockManagementService.handleOneBlock(blockJson);
                 }
 
             }
@@ -140,19 +142,20 @@ public class BlockHandlerThread extends Thread {
             try {
                 remoteHeight = ConstantParam.ONT_SDKSERVICE.getConnect().getBlockHeight();
                 break;
-            } catch (Exception ex) {
+            } catch (ConnectorException ex) {
                 logger.error("getBlockHeight error, try again...restful:{},error:", ConstantParam.MASTERNODE_RESTFULURL, ex);
-                if (tryTime < ConstantParam.NODE_RETRYMAXTIME && tryTime % configParam.NODE_INTERRUPTTIME_MAX == 0) {
+                if (tryTime % configParam.NODE_INTERRUPTTIME_MAX == 0) {
                     switchNode();
                     tryTime++;
                     continue;
-                } else if (tryTime < ConstantParam.NODE_RETRYMAXTIME) {
+                } else {
                     tryTime++;
                     Thread.sleep(1000);
                     continue;
                 }
-                logger.error("get blockheight thread can't work,error {} ", ex);
-                throw new Exception(ex);
+            } catch (IOException e) {
+                logger.error("get blockheight thread can't work,error {} ", e);
+                throw new Exception(e);
             }
         }
 
@@ -174,17 +177,19 @@ public class BlockHandlerThread extends Thread {
             try {
                 block = (JSONObject) ConstantParam.ONT_SDKSERVICE.getConnect().getBlockJson(height);
                 break;
-            } catch (Exception ex) {
+            } catch (ConnectorException ex) {
                 logger.error("getBlockJsonByHeight error, try again...restful:{},error:", ConstantParam.MASTERNODE_RESTFULURL, ex);
-                if (tryTime < ConstantParam.NODE_RETRYMAXTIME && tryTime % configParam.NODE_INTERRUPTTIME_MAX == 0) {
+                if (tryTime % configParam.NODE_INTERRUPTTIME_MAX == 0) {
                     switchNode();
                     tryTime++;
                     continue;
-                } else if (tryTime < ConstantParam.NODE_RETRYMAXTIME) {
+                } else {
                     tryTime++;
                     Thread.sleep(1000);
                     continue;
                 }
+
+            } catch (IOException ex) {
                 logger.error("getBlockJsonByHeight thread can't work,error {} ", ex);
                 throw new Exception(ex);
             }
@@ -209,17 +214,18 @@ public class BlockHandlerThread extends Thread {
                 JSONObject blockJson = (JSONObject) ConstantParam.ONT_SDKSERVICE.getConnect().getBlockJson(height);
                 nextBookKeeper = blockJson.getJSONObject("Header").getString("NextBookkeeper");
                 break;
-            } catch (Exception ex) {
+            } catch (ConnectorException ex) {
                 logger.error("getBlockBookKeeperByHeight error, try again...restsful:{},error:", ConstantParam.MASTERNODE_RESTFULURL, ex);
-                if (tryTime < ConstantParam.NODE_RETRYMAXTIME && tryTime % configParam.NODE_INTERRUPTTIME_MAX == 0) {
+                if (tryTime % configParam.NODE_INTERRUPTTIME_MAX == 0) {
                     switchNode();
                     tryTime++;
                     continue;
-                } else if (tryTime < ConstantParam.NODE_RETRYMAXTIME) {
+                } else {
                     tryTime++;
                     Thread.sleep(1000);
                     continue;
                 }
+            } catch (IOException ex) {
                 logger.error("getBlockBookKeeperByHeight thread can't work,error {} ", ex);
                 throw new Exception(ex);
             }
