fc8e4713b3921b2cace6ea83c4a3e10fae5c4353
zzsZhou <zhouqiang@onchain.com>
2018-09-21 16:49:52 +0800

    add addrcount

--- back-end/src/main/java/com/github/ontio/service/impl/CurrentServiceImpl.java
+++ back-end/src/main/java/com/github/ontio/service/impl/CurrentServiceImpl.java
@@ -17,10 +17,10 @@
  */
 
 
-
 package com.github.ontio.service.impl;
 
 import com.github.ontio.dao.CurrentMapper;
+import com.github.ontio.dao.TransactionDetailMapper;
 import com.github.ontio.paramBean.Result;
 import com.github.ontio.service.ICurrentService;
 import com.github.ontio.utils.ConfigParam;
@@ -34,6 +34,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -45,12 +46,14 @@ import java.util.Map;
 @MapperScan("com.github.ontio.dao")
 public class CurrentServiceImpl implements ICurrentService {
 
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private static final Logger logger = LoggerFactory.getLogger(CurrentServiceImpl.class);
 
     private static final String VERSION = "1.0";
 
     @Autowired
     private CurrentMapper currentMapper;
+    @Autowired
+    TransactionDetailMapper transactionDetailMapper;
     @Autowired
     private ConfigParam configParam;
 
@@ -66,15 +69,18 @@ public class CurrentServiceImpl implements ICurrentService {
     public Result querySummaryInfo() {
 
         Map summary = currentMapper.selectSummaryInfo();
+        List<String> addrList = transactionDetailMapper.selectAllAddress();
 
         initSDK();
         int nodeCount = sdk.getNodeCount();
 
         Map<String, Object> rs = new HashMap();
+
         rs.put("NodeCount", nodeCount);
         rs.put("CurrentHeight", summary.get("Height"));
         rs.put("TxnCount", summary.get("TxnCount"));
         rs.put("OntIdCount", summary.get("OntIdCount"));
+        rs.put("AddrCount", addrList.size());
 
         return Helper.result("QueryCurrentInfo", ErrorInfo.SUCCESS.code(), ErrorInfo.SUCCESS.desc(), VERSION, rs);
     }
