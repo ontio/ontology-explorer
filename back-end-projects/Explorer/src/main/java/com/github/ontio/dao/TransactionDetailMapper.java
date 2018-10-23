0b8f4f80af8e07f701862b8cd61a780876352f24
zzsZhou <zhouqiang@onchain.com>
2018-10-23 14:44:14 +0800

    add query
    Former-commit-id: d7bede8675c03f6c779bb078edf826e70d838e67

--- back-end-projects/Explorer/src/main/java/com/github/ontio/dao/TransactionDetailMapper.java
+++ back-end-projects/Explorer/src/main/java/com/github/ontio/dao/TransactionDetailMapper.java
@@ -3,7 +3,6 @@ package com.github.ontio.dao;
 import com.github.ontio.model.TransactionDetail;
 import com.github.ontio.model.TransactionDetailKey;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -32,8 +31,6 @@ public interface TransactionDetailMapper {
 
     int selectTxnWithoutOntIdAmount();
 
-    int selectTxnCountInOneDay(int startTime,int endTime);
-
     Map<String,Object> selectTxnByHash(String txnHash);
 
     List<Map> selectTxnByBlockHeight(int height);
@@ -48,13 +45,19 @@ public interface TransactionDetailMapper {
 
     List<Map> selectTxnByAddressInfo(Map<String, Object> param);
 
+    List<Map> selectTxnByToAddressInfo(Map<String, Object> param);
+
+    List<Map> selectTxnByFromAddressInfo(Map<String, Object> param);
+
+
     List<Map> selectTxnByAddressInfoAndTimePage(Map<String, Object> param);
 
     List<Map> selectTxnByAddressInfoAndTime(Map<String, Object> param);
 
     int selectTxnAmountByAddressInfo(Map<String, Object> param);
 
+    List<String> selectTxnAmountByAddressInfo2(Map<String, Object> param);
+
     List<String> selectAllAddress();
 
-    List<Map> selectLatestTransferTxnAddr(@Param("beginIndex") int beginIndex, @Param("amount") int amount);
 }
