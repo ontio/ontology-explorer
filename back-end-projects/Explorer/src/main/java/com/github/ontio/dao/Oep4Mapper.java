0ca54619220817b14025c26d3fdf96d1d5c75dc3
zzsZhou <zhouqiang@onchain.com>
2018-11-12 18:36:23 +0800

    add

---
+++ back-end-projects/Explorer/src/main/java/com/github/ontio/dao/Oep4Mapper.java
@@ -0,0 +1,25 @@
+package com.github.ontio.dao;
+
+import com.github.ontio.model.Oep4;
+import com.github.ontio.model.Oep4Key;
+
+import java.util.List;
+import java.util.Map;
+
+public interface Oep4Mapper {
+    int deleteByPrimaryKey(Oep4Key key);
+
+    int insert(Oep4 record);
+
+    int insertSelective(Oep4 record);
+
+    Oep4 selectByPrimaryKey(Oep4Key key);
+
+    int updateByPrimaryKeySelective(Oep4 record);
+
+    int updateByPrimaryKey(Oep4 record);
+
+
+    List<Map> selectAllKeyInfo();
+
+}
