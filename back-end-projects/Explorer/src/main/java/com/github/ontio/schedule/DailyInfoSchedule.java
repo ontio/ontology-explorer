5a412610c74e34bba43a177356cfe8f0bd702319
zzsZhou <zhouqiang@onchain.com>
2018-12-11 11:45:41 +0800

    add UpdateContractInfo

--- back-end-projects/Explorer/src/main/java/com/github/ontio/schedule/DailyInfoSchedule.java
+++ back-end-projects/Explorer/src/main/java/com/github/ontio/schedule/DailyInfoSchedule.java
@@ -1,9 +1,8 @@
 package com.github.ontio.schedule;
 
-import com.github.ontio.dao.BlockMapper;
-import com.github.ontio.dao.DailyMapper;
-import com.github.ontio.dao.OntIdMapper;
-import com.github.ontio.dao.TransactionDetailMapper;
+import com.github.ontio.common.Address;
+import com.github.ontio.dao.*;
+import com.github.ontio.model.Contracts;
 import com.github.ontio.model.Daily;
 import com.github.ontio.utils.Helper;
 import org.slf4j.Logger;
@@ -13,7 +12,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
+import java.math.BigDecimal;
+import java.util.*;
 
 /**
  * @author zhouq
@@ -23,7 +23,6 @@ import java.util.Date;
 @Component
 @EnableScheduling
 public class DailyInfoSchedule {
-
 
     private static final Logger logger = LoggerFactory.getLogger(DailyInfoSchedule.class);
 
@@ -37,8 +36,13 @@ public class DailyInfoSchedule {
     private OntIdMapper ontIdMapper;
     @Autowired
     private DailyMapper dailyMapper;
+    @Autowired
+    private ContractsMapper contractsMapper;
 
 
+    /**
+     * 记录每日统计数据
+     */
     @Scheduled(cron = "0 0 0 * * *")
     public void UpdateDailyInfo() {
 
@@ -49,17 +53,86 @@ public class DailyInfoSchedule {
 
         int blockCount = blockMapper.selectBlockCountInOneDay(startTime, endTime);
         int txnCount = transactionDetailMapper.selectTxnCountInOneDay(startTime, endTime);
-
         String ontIdDes = "Register%";
         int ontidCount = ontIdMapper.selectOntIdCountInOneDay(startTime, endTime, ontIdDes);
+
+        Set<String> addressSet = new HashSet<>();
+
+        List<Map<String, String>> recordList = transactionDetailMapper.selectAllAddress();
+        for (Map<String, String> addressMap :
+                recordList) {
+            for (String key :
+                    addressMap.keySet()) {
+                addressSet.add(key);
+                addressSet.add(addressMap.get(key));
+            }
+        }
+
 
         Daily dailyDAO = new Daily();
         dailyDAO.setBlockcount(blockCount);
         dailyDAO.setTxncount(txnCount);
         dailyDAO.setOntidcount(ontidCount);
+        dailyDAO.setAddresscount(addressSet.size());
         dailyDAO.setTime(new Date(startTime * 1000L));
-
         dailyMapper.insert(dailyDAO);
+    }
+
+
+    /**
+     * 记录合约统计数据
+     */
+    @Scheduled(cron = "0/20 * * * * *")
+    public void UpdateContractInfo() {
+
+        logger.info("####{}.{} begin...", CLASS_NAME, Helper.currentMethod());
+
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("Start", 0);
+        paramMap.put("PageSize", 40);
+        List<Map> contractList = contractsMapper.selectContractByPage(paramMap);
+
+        for (Map map :
+                contractList) {
+            try {
+                String contractHash = (String) map.get("ContractHash");
+                String address = Address.parse(com.github.ontio.common.Helper.reverse(contractHash)).toBase58();
+                logger.info("contractHash:{},address:{}", contractHash, address);
+
+                Map<String, Object> paramMap1 = new HashMap<>();
+                paramMap1.put("address", address);
+                paramMap1.put("assetname", "ont");
+                BigDecimal ontCount = transactionDetailMapper.selectContractAssetSum(paramMap1);
+
+                paramMap1.put("assetname", "ong");
+                BigDecimal ongCount = transactionDetailMapper.selectContractAssetSum(paramMap1);
+
+                List<String> fromAddrList = transactionDetailMapper.selectAllFromAddress(address);
+                List<String> toAddrList = transactionDetailMapper.selectAllToAddress(address);
+                Set<String> addrSet = new HashSet<>();
+                for (String str :
+                        fromAddrList) {
+                    addrSet.add(str);
+                }
+                for (String str :
+                        toAddrList) {
+                    addrSet.add(str);
+                }
+
+                int txnCount = transactionDetailMapper.selectContractAddrAmount(contractHash);
+
+                Contracts contractsDAO = new Contracts();
+                contractsDAO.setContract(contractHash);
+                contractsDAO.setTxcount(txnCount);
+                contractsDAO.setOngcount(ongCount);
+                contractsDAO.setOntcount(ontCount);
+                contractsDAO.setAddresscount(addrSet.size());
+                contractsMapper.updateByPrimaryKeySelective(contractsDAO);
+            } catch (Exception e) {
+                logger.error("error...", e);
+            }
+        }
+
     }
 
 
