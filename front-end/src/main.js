c213e30939ff410e5efb994e32818a687f6b7d29
LiYingxuan <mvp_xuan@163.com>
2019-01-02 18:19:13 +0800

    Upgrade translation component.

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -7,8 +7,7 @@ import router from './router'
 import store from './store/index'
 import Axios from 'axios'
 import VueAxios from 'vue-axios'
-import VueI18n from 'vue-i18n'
-import LangStorage from './helpers/lang'
+import i18n from "./common/lang"; // Internationalization
 import $ from 'jquery'
 import 'font-awesome/css/font-awesome.css'
 
@@ -16,11 +15,9 @@ import Helper from './helpers/helper'
 Vue.use(Helper);
 
 import {
-  // Button,
   Pagination
 } from "element-ui";
 Vue.component(Pagination.name, Pagination);
-// Vue.component(Button.name, Button);
 
 /**
  * 全局组件注册
@@ -47,30 +44,9 @@ Vue.component('line-chart', LineChart);
  * Fixed compatibility issues with low version IE. lyx.
  */
 import promise from 'es6-promise'
-
 promise.polyfill();
 
-/**
- * Vee Validate
- * Front-end Input need the field：data-vv-as
- */
-import zh_CN from 'vee-validate/dist/locale/zh_CN'
-import VeeValidate, {Validator} from 'vee-validate'
-// Validator.localize('zh_CN', zh_CN);
-Vue.use(VeeValidate, {
-  // locale: 'zh_CN'
-});
-
 Vue.use(VueAxios, Axios);
-
-Vue.use(VueI18n);
-const i18n = new VueI18n({
-  locale: LangStorage.getLang('en'),  // 语言标识
-  messages: {
-    'zh': require('./common/lang/zh'),
-    'en': require('./common/lang/en')
-  }
-});
 
 Vue.config.productionTip = false;
 
