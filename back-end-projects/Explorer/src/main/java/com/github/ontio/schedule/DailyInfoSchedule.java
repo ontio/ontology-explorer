f0401c278602fd6b8292c0533bccb65f8df21689
zzsZhou <zhouqiang@onchain.com>
2019-01-14 13:29:20 +0800

    更新合约信息统计逻辑

--- back-end-projects/Explorer/src/main/java/com/github/ontio/schedule/DailyInfoSchedule.java
+++ back-end-projects/Explorer/src/main/java/com/github/ontio/schedule/DailyInfoSchedule.java
@@ -78,21 +78,22 @@ public class DailyInfoSchedule {
                 startTime = 1530288000;
             }
 
-            // 将每10分钟的数据先转移到临时表，再统计其他信息
+            //每天0:10会将daily表前一天数据清掉
             Map param = new HashMap<>();
             param.put("startTime", startTime + 24 * 60 * 60);
             if (transactionDetailDailyMapper.selectiveByEndTime(param) != 0){
                 transactionDetailDailyMapper.deleteByEndTime(param);
             }
 
-            //查询所有contract
+            //查询所有审核过的contract
             List<Contracts> contractList = contractsMapper.selectAllApprovedContract();
-            for (Contracts contracts : contractList) {
+            for (Contracts contracts :
+                    contractList) {
                 String type = contracts.getType();
                 String contractHash = contracts.getContract();
                 String contractAddress = Address.parse(com.github.ontio.common.Helper.reverse(contractHash)).toBase58();
                 logger.info("contractHash:{}, contractAddress:{}", contractHash, contractAddress);
-
+                //查询合约到目前为止的统计数据
                 ContractSummary contractSummary = contractSummaryMapper.selectContractSummary(contractHash);
                 if (contractSummary == null){
                     contractSummary = new ContractSummary();
@@ -106,33 +107,26 @@ public class DailyInfoSchedule {
                 paramMap.put("contractHash", contractHash);
                 paramMap.put("contractAddress", contractAddress);
                 paramMap.put("assetname", "ont");
-                BigDecimal ontCount = transactionDetailDailyMapper.selectContractAssetSum(paramMap);
+                BigDecimal ontCount = transactionDetailDailyMapper.selectContractAssetSumNew(paramMap);
                 ontCount = ontCount == null ? new BigDecimal(0) : ontCount;
 
                 paramMap.put("assetname", "ong");
-                BigDecimal ongCount = transactionDetailDailyMapper.selectContractAssetSum(paramMap);
+                BigDecimal ongCount = transactionDetailDailyMapper.selectContractAssetSumNew(paramMap);
                 ongCount = ongCount == null ? new BigDecimal(0) : ongCount.divide(new BigDecimal("1000000000"));
 
-                // 依据合约hash和合约地址分别查询交易数
+                // 依据合约hash查询交易数
                 int txnCount = transactionDetailDailyMapper.selectTxnAmount(paramMap);
 
                 // 依据合约hash和合约地址分别查询地址数（去重）
-                List<String> addressByContractList = transactionDetailDailyMapper.selectAllAddressByContract(contractHash);
-                List<String> addressByAddressList = transactionDetailDailyMapper.selectAllAddressByAddress(contractAddress);
-                addressByContractList.addAll(addressByAddressList);
+                List<String> toAddrList = transactionDetailDailyMapper.selectToAddressCountByContractNew(contractHash);
+                List<String> fromAddrList = transactionDetailDailyMapper.selectFromAddressCountByContractNew(contractAddress);
+                toAddrList.addAll(fromAddrList);
 
-                List<String> addressList = new ArrayList<>();
-                for (String address: addressByContractList) {
-                    if (address.length() != 34){
-                        continue;
-                    }
+                List<String> addrList = toAddrList;
 
-                    addressList.add(address);
-                }
-
-                // 计算新增地址数
+                // 新增地址数=合约活跃地址数-address_summary所有地址
                 List<String> contractAddressList = addressSummaryMapper.selectDistinctAddressByContract(contractHash);
-                addressList.removeAll(contractAddressList);
+                addrList.removeAll(contractAddressList);
 
                 // 计算合约内部转账金额
                 List<Map> tokenCountMapList = null;
@@ -155,7 +149,7 @@ public class DailyInfoSchedule {
                 contracts.setOngcount(ongCount.add(contractSummary.getOngcount()));
                 contracts.setOntcount(ontCount.add(contractSummary.getOntcount()));
                 contracts.setTokencount(JSON.toJSONString(tokenCountMapList == null ? new ArrayList<>() : tokenCountMapList));
-                contracts.setAddresscount(contractSummary.getNewaddress() + addressList.size());
+                contracts.setAddresscount(contractSummary.getNewaddress() + addrList.size());
             }
 
             contractsMapper.banchUpdateByPrimaryKeySelective(contractList);
