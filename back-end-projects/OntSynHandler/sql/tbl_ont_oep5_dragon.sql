1058ea57d100246d7184c1f03db90b941aff3ebf
zzsZhou <18217008370@163.com>
2019-04-12 16:57:31 +0800

    整理sql
    Former-commit-id: 5a6d77cb9ba88ae39e5101b7941760878e91410a

--- back-end-projects/OntSynHandler/sql/tbl_ont_oep5_dragon.sql
+++ back-end-projects/OntSynHandler/sql/tbl_ont_oep5_dragon.sql
@@ -1,16 +1,13 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : 
-Source Server Version : 50639
-Source Host           : 
-Source Database       : explorer
+
 
 Target Server Type    : MYSQL
 Target Server Version : 50639
 File Encoding         : 65001
 
-Date: 2019-04-01 11:30:56
+Date: 2019-04-12 11:00:33
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -21,8 +18,8 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `tbl_ont_oep5_dragon`;
 CREATE TABLE `tbl_ont_oep5_dragon` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `contract` varchar(255) NOT NULL DEFAULT '',
-  `assertname` varchar(255) NOT NULL DEFAULT '',
-  `jsonurl` varchar(255) NOT NULL,
+  `contract` varchar(255) NOT NULL DEFAULT '' COMMENT '合约hash',
+  `assertname` varchar(255) NOT NULL DEFAULT '' COMMENT '该dragon的name',
+  `jsonurl` varchar(255) NOT NULL COMMENT '该dragon的url',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2131 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2322 DEFAULT CHARSET=utf8;
