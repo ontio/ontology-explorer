5a6d77cb9ba88ae39e5101b7941760878e91410a
zzsZhou <18217008370@163.com>
2019-04-12 16:57:31 +0800

    整理sql

--- back-end-projects/OntSynHandler/sql/tbl_ont_oep8.sql
+++ back-end-projects/OntSynHandler/sql/tbl_ont_oep8.sql
@@ -1,16 +1,13 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : 
-Source Server Version : 50639
-Source Host           : 
-Source Database       : explorer
+
 
 Target Server Type    : MYSQL
 Target Server Version : 50639
 File Encoding         : 65001
 
-Date: 2019-04-01 11:31:14
+Date: 2019-04-12 11:01:01
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -22,16 +19,16 @@ DROP TABLE IF EXISTS `tbl_ont_oep8`;
 CREATE TABLE `tbl_ont_oep8` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `contract` varchar(255) NOT NULL DEFAULT '',
-  `tokenid` varchar(255) NOT NULL DEFAULT '',
-  `name` varchar(255) NOT NULL DEFAULT '',
-  `totalsupply` decimal(30,0) NOT NULL,
-  `symbol` varchar(255) NOT NULL DEFAULT '',
-  `description` varchar(255) NOT NULL DEFAULT '',
-  `contactinfo` varchar(2000) NOT NULL DEFAULT '',
+  `tokenid` varchar(255) NOT NULL DEFAULT '' COMMENT '该oep8的tokenid',
+  `name` varchar(255) NOT NULL DEFAULT '' COMMENT '该oep8的name',
+  `totalsupply` decimal(30,0) NOT NULL COMMENT '该oep8 tokenid的总发行量',
+  `symbol` varchar(255) NOT NULL DEFAULT '' COMMENT '该oep8 tokenid的符号',
+  `description` varchar(255) NOT NULL DEFAULT '' ,
+  `contactinfo` varchar(2000) NOT NULL DEFAULT '' COMMENT '联系信息',
   `createtime` datetime NOT NULL,
-  `auditflag` int(1) NOT NULL DEFAULT '0',
+  `auditflag` int(1) NOT NULL DEFAULT '0' COMMENT '审核标识 1：审核通过 0：未审核',
   `updatetime` datetime DEFAULT NULL,
-  `logo` varchar(255) NOT NULL DEFAULT '',
+  `logo` varchar(255) NOT NULL DEFAULT '' COMMENT '该oep8token的logo的url',
   PRIMARY KEY (`id`),
   KEY `idx_contract` (`contract`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
