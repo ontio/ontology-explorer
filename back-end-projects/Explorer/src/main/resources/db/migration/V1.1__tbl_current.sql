f025e28f6c0d4861b2460fc34790d91abfb6c12b
zzsZhou <18217008370@163.com>
2019-04-25 20:05:17 +0800

    添加swagger2
    Former-commit-id: 86f8c37ba6269d38e69bb9065b50cf2d916ec028

---
+++ back-end-projects/Explorer/src/main/resources/db/migration/V1.1__tbl_current.sql
@@ -0,0 +1,27 @@
+/*
+Navicat MySQL Data Transfer
+
+
+
+Target Server Type    : MYSQL
+Target Server Version : 50639
+File Encoding         : 65001
+
+Date: 2019-04-12 10:56:14
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for tbl_ont_current
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_current`;
+CREATE TABLE `tbl_current` (
+  `block_height` int(11) NOT NULL COMMENT '当前同步的最新区块高度',
+  `tx_count` int(11) NOT NULL COMMENT '当前同步的最新交易数量',
+  `ontid_count` int(11) NOT NULL COMMENT '当前同步的最新ONT ID数量',
+  `nonontid_tx_count` int(11) NOT NULL COMMENT '当前同步的最新非ONT ID相关的交易数量'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+insert into tbl_current(block_height,tx_count,ontid_count,nonontid_tx_count) values(-1,0,0,0);
