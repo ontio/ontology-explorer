786dd711227cffbd0fb540ff4aa732f18c762f0a
zzsZhou <18217008370@163.com>
2019-04-25 16:35:25 +0800

    v2 create tables sql
    Former-commit-id: 5bc72bca16514c0f6fbfa38106ffe9c8f7f25f05

--- back-end-projects/Explorer/sql/tbl_address_daily_summary.sql
+++ back-end-projects/Explorer/sql/tbl_address_daily_summary.sql
@@ -15,11 +15,11 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 -- Table structure for tbl_ont_address_summary
 -- ----------------------------
-DROP TABLE IF EXISTS `tbl_ont_address_summary`;
-CREATE TABLE `tbl_ont_address_summary` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `tbl_address_daily_summary`;
+CREATE TABLE `tbl_address_daily_summary` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `time` int(11) NOT NULL COMMENT '当天UTC0点时间戳',
-  `type` varchar(64) NOT NULL COMMENT 'common:原生资产地址 contracthash：合约地址',
+  `contract_hash` varchar(64) NOT NULL COMMENT '合约hash值',
   `address` varchar(64) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`) USING BTREE,
   KEY `idx_time` (`time`) USING BTREE
