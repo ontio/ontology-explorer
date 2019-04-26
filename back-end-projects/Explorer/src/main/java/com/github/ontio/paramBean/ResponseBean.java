c70e2b4c32d4b15e56041c840733cb41c96d04dd
zzsZhou <18217008370@163.com>
2019-04-26 15:48:28 +0800

    response dto
    Former-commit-id: f4b09d66b42f5d80f8d91a816d1b8a006190fd76

---
+++ back-end-projects/Explorer/src/main/java/com/github/ontio/paramBean/ResponseBean.java
@@ -0,0 +1,26 @@
+package com.github.ontio.paramBean;
+
+import lombok.Data;
+
+/**
+ * @author zhouq
+ * @version 1.0
+ * @date 2019/4/26
+ */
+@Data
+public class ResponseBean {
+
+    private Integer code;
+
+    private String msg;
+
+    private Object result;
+
+    public ResponseBean(Integer code, String msg, Object result){
+        this.code = code;
+        this.msg = msg;
+        this.result = result;
+    }
+
+
+}
