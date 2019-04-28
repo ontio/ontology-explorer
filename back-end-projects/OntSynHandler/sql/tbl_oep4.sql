e4491e27fd870f7dac9955911778109c61854e8b
dingxin <wdx7266@vip.qq.com>
2019-04-28 11:25:47 +0800

    format sql
    Former-commit-id: 76ba409775cfa81c4b786e0d65c3d89853e15371

--- back-end-projects/OntSynHandler/sql/tbl_oep4.sql
+++ back-end-projects/OntSynHandler/sql/tbl_oep4.sql
@@ -10,20 +10,22 @@ File Encoding         : 65001
 Date: 2019-04-12 10:59:58
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for tbl_ont_oep4
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_oep4`;
-CREATE TABLE `tbl_oep4` (
-  `contract_hash` varchar(255) NOT NULL DEFAULT '' COMMENT '合约hash值',
-  `name` varchar(255) NOT NULL DEFAULT '' COMMENT 'OEP4代币名称',
-  `total_supply` decimal(15,0) NOT NULL COMMENT 'OEP4代币总量',
-  `symbol` varchar(255) NOT NULL DEFAULT '' COMMENT 'OEP4代币符号',
-  `decimals` int(11) NOT NULL COMMENT 'OEP4代币精度',
-  `create_time` datetime COMMENT '创建时间' NOT NULL,
-  `audit_flag` int(11) NOT NULL COMMENT '审核标识，1：审核通过 0：未审核',
-  `update_time` datetime COMMENT '更新时间'  DEFAULT NULL,
-  PRIMARY KEY (`contract_hash`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `tbl_oep4`
+(
+    `contract_hash` varchar(255)            NOT NULL DEFAULT '' COMMENT '合约hash值',
+    `name`          varchar(255)            NOT NULL DEFAULT '' COMMENT 'OEP4代币名称',
+    `total_supply`  decimal(15, 0)          NOT NULL COMMENT 'OEP4代币总量',
+    `symbol`        varchar(255)            NOT NULL DEFAULT '' COMMENT 'OEP4代币符号',
+    `decimals`      int(11)                 NOT NULL COMMENT 'OEP4代币精度',
+    `create_time`   datetime COMMENT '创建时间' NOT NULL,
+    `audit_flag`    int(11)                 NOT NULL COMMENT '审核标识，1：审核通过 0：未审核',
+    `update_time`   datetime COMMENT '更新时间'          DEFAULT NULL,
+    PRIMARY KEY (`contract_hash`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
