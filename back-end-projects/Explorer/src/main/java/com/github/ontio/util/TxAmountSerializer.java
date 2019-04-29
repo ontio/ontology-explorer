52cce94d15315b1ba395b9f16d39b07a10fa61f6
zzsZhou <18217008370@163.com>
2019-04-29 15:28:43 +0800

    update
    Former-commit-id: e83bc035d8232cc51c5e25af75ecf3931e41115d

--- back-end-projects/Explorer/src/main/java/com/github/ontio/util/TxAmountSerializer.java
+++ back-end-projects/Explorer/src/main/java/com/github/ontio/util/TxAmountSerializer.java
@@ -11,5 +11,28 @@ public class TxAmountSerializer extends JsonSerializer<BigDecimal> {
     @Override
     public void serialize(BigDecimal bigDecimal, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
         jsonGenerator.writeString(bigDecimal.stripTrailingZeros().toPlainString());
+
+
+/*        jsonGenerator.writeStartObject();
+        jsonGenerator.writeFieldName("id");
+        jsonGenerator.writeString("自定义");
+        jsonGenerator.writeObjectField("buildDate",bigDecimal.divide(new BigDecimal("10")));
+        jsonGenerator.writeEndObject();*/
     }
+
+/*
+    @Override
+    public void serializeWithType(BigDecimal value, JsonGenerator gen,
+                                  SerializerProvider provider, TypeSerializer typeSer)
+            throws IOException, JsonProcessingException {
+
+        typeSer.writeTypePrefixForObject(value, gen);
+        serialize(value, gen, provider); // call your customized serialize method
+        typeSer.writeTypeSuffixForObject(value, gen);
+    }
+*/
+
+
+
+
 }
