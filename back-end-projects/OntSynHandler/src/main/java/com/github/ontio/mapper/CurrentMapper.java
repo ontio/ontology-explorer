d714755c9fb971da1a32261a9c6debd2cb650e40
zzsZhou <18217008370@163.com>
2019-05-06 17:46:32 +0800

    update
    Former-commit-id: c4aa518e84246506cbdc9147e5eed42308282f57

--- back-end-projects/OntSynHandler/src/main/java/com/github/ontio/mapper/CurrentMapper.java
+++ back-end-projects/OntSynHandler/src/main/java/com/github/ontio/mapper/CurrentMapper.java
@@ -4,4 +4,9 @@ import com.github.ontio.model.dao.Current;
 import tk.mybatis.mapper.common.Mapper;
 
 public interface CurrentMapper extends Mapper<Current> {
+
+    void update(Current current);
+
+    Integer selectBlockHeight();
+
 }
