fe97003e3ab672a479cc0ba49856f299db2889f8
zzsZhou <18217008370@163.com>
2019-05-08 19:12:58 +0800

    新增token相关api
    Former-commit-id: 267be718bff114a5709cacba4456fecebdd23cd2

---
+++ back-end-projects/Explorer/src/main/java/com/github/ontio/model/dto/Oep5DetailDto.java
@@ -0,0 +1,25 @@
+package com.github.ontio.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.github.ontio.model.dao.Contract;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+/**
+ * @author zhouq
+ * @version 1.0
+ * @date 2019/5/8
+ */
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Oep5DetailDto extends Contract {
+
+    private BigDecimal totalSupply;
+
+    private String symbol;
+
+    private String name;
+
+
+}
---
+++ back-end-projects/Explorer/src/main/java/com/github/ontio/service/ITokenService.java
@@ -0,0 +1,16 @@
+package com.github.ontio.service;
+
+import com.github.ontio.model.common.ResponseBean;
+
+/**
+ * @author zhouq
+ * @version 1.0
+ * @date 2019/5/8
+ */
+public interface ITokenService {
+
+    ResponseBean queryTokensByPage(String tokenType, Integer pageNumber, Integer pageSize);
+
+    ResponseBean queryOep8TxsByPage(String contractHash, String tokenName, Integer pageNumber, Integer pageSize);
+
+}
