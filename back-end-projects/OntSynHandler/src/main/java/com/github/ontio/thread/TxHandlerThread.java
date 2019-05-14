070d6a76d35587de9e90630f52ec5fef7e0fc64e
zzsZhou <18217008370@163.com>
2019-05-14 13:33:28 +0800

    处理oep4转账判断action

--- back-end-projects/OntSynHandler/src/main/java/com/github/ontio/thread/TxHandlerThread.java
+++ back-end-projects/OntSynHandler/src/main/java/com/github/ontio/thread/TxHandlerThread.java
@@ -909,17 +909,18 @@ public class TxHandlerThread {
 
         } else {
             String action = new String(Helper.hexToBytes((String) stateArray.get(0)));
-            if (action.equalsIgnoreCase("transfer")) {
-                fromAddress = (String) stateArray.get(1);
-                if (40 == fromAddress.length()) {
-                    fromAddress = Address.parse(fromAddress).toBase58();
-                }
-                toAddress = (String) stateArray.get(2);
-                if (40 == toAddress.length()) {
-                    toAddress = Address.parse(toAddress).toBase58();
-                }
-                eventAmount = new BigDecimal(Helper.BigIntFromNeoBytes(Helper.hexToBytes((String) stateArray.get(3))).longValue());
+            if (!action.equalsIgnoreCase("transfer")) {
+                return;
             }
+            fromAddress = (String) stateArray.get(1);
+            if (40 == fromAddress.length()) {
+                fromAddress = Address.parse(fromAddress).toBase58();
+            }
+            toAddress = (String) stateArray.get(2);
+            if (40 == toAddress.length()) {
+                toAddress = Address.parse(toAddress).toBase58();
+            }
+            eventAmount = new BigDecimal(Helper.BigIntFromNeoBytes(Helper.hexToBytes((String) stateArray.get(3))).longValue());
         }
         log.info("OEP4TransferTx:fromaddress:{}, toaddress:{}, amount:{}", fromAddress, toAddress, eventAmount);
 
