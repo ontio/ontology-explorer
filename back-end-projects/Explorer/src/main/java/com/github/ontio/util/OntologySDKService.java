8070afdae4e39f9a20b305377b0f5644d2fcb243
zzsZhou <18217008370@163.com>
2019-05-14 17:55:58 +0800

    删除无用方法

--- back-end-projects/Explorer/src/main/java/com/github/ontio/util/OntologySDKService.java
+++ back-end-projects/Explorer/src/main/java/com/github/ontio/util/OntologySDKService.java
@@ -22,12 +22,9 @@ package com.github.ontio.util;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import com.github.ontio.OntSdk;
-import com.github.ontio.common.Helper;
-import com.github.ontio.config.ConfigParam;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.github.ontio.config.ParamsConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -39,25 +36,24 @@ import java.util.Map;
  * @author zhouq
  * @date 2018/2/27
  */
-
+@Slf4j
 @Component
 public class OntologySDKService {
-    private static final Logger logger = LoggerFactory.getLogger(OntologySDKService.class);
-
+    
     private static OntologySDKService instance = null;
 
     @Autowired
-    private ConfigParam configParam;
+    private ParamsConfig paramsConfig;
 
-    public static synchronized OntologySDKService getInstance(ConfigParam param) {
+    public static synchronized OntologySDKService getInstance(ParamsConfig param) {
         if (instance == null) {
             instance = new OntologySDKService(param);
         }
         return instance;
     }
 
-    public OntologySDKService(ConfigParam param) {
-        this.configParam = param;
+    public OntologySDKService(ParamsConfig param) {
+        this.paramsConfig = param;
     }
 
     public String getDDO(String ontId) {
@@ -66,25 +62,13 @@ public class OntologySDKService {
         try {
             ddoStr = ontSdk.nativevm().ontId().sendGetDDO(ontId);
         } catch (Exception e) {
-            logger.error("get ddo error...", e);
+            log.error("getDDO error...", e);
             e.printStackTrace();
         }
 
         return ddoStr;
     }
 
-    public int getNodeCount() {
-        OntSdk ontSdk = getOntSdk();
-        int nodeCount = 0;
-        try {
-            nodeCount = ontSdk.getConnect().getNodeCount();
-        } catch (Exception e) {
-            logger.error("get nodecount error...", e);
-            e.printStackTrace();
-        }
-
-        return nodeCount;
-    }
 
     /**
      * 获取账户余额，包括unboundong
@@ -98,7 +82,7 @@ public class OntologySDKService {
             OntSdk ontSdk = getOntSdk();
             balanceMap = (Map) ontSdk.getConnect().getBalance(address);
         } catch (Exception e) {
-            logger.error("getNativeAssetBalance error...", e);
+            log.error("getNativeAssetBalance error...", e);
             balanceMap.put("ong", "0");
             balanceMap.put("ont", "0");
         }
@@ -117,7 +101,7 @@ public class OntologySDKService {
             String balance = ontSdk.neovm().oep4().queryBalanceOf(address);
             return balance;
         } catch (Exception e) {
-            logger.error("getOep4AssetBalance error...", e);
+            log.error("getOep4AssetBalance error...", e);
             return "0";
         }
     }
@@ -134,7 +118,7 @@ public class OntologySDKService {
             String balance = ontSdk.neovm().oep5().queryBalanceOf(address);
             return balance;
         } catch (Exception e) {
-            logger.error("getOep5AssetBalance error...", e);
+            log.error("getOep5AssetBalance error...", e);
             return "0";
         }
     }
@@ -152,7 +136,7 @@ public class OntologySDKService {
             String balance = ontSdk.neovm().oep8().balancesOf(address);
             balanceArray = JSON.parseArray(balance);
         } catch (Exception e) {
-            logger.error("getOpe8AssetBalance error...", e);
+            log.error("getOpe8AssetBalance error...", e);
         }
         return balanceArray;
     }
@@ -169,102 +153,39 @@ public class OntologySDKService {
             String unboundOng = ontSdk.nativevm().ong().unboundOng(address);
             return new BigDecimal(unboundOng).divide(ConstantParam.ONT_TOTAL).toPlainString();
         } catch (Exception e) {
-            logger.error("getNativeAssetBalance error...", e);
+            log.error("getUnBoundOng error...", e);
             e.printStackTrace();
             return null;
         }
     }
+    
 
-    public JSONObject queryOep4Info(String contractHash) {
-        try {
-            OntSdk ontSdk = getOep4OntSdk(contractHash);
-            String name = ontSdk.neovm().oep4().queryName();
-            String symbol = ontSdk.neovm().oep4().querySymbol();
-            String decimal = ontSdk.neovm().oep4().queryDecimals();
-            String total = ontSdk.neovm().oep4().queryTotalSupply();
-
-            JSONObject oep4Obj = new JSONObject();
-            oep4Obj.put("Name", name);
-            oep4Obj.put("Symbol", symbol);
-            oep4Obj.put("TotalSupply", total);
-            oep4Obj.put("Decimal", decimal);
-            return oep4Obj;
-        } catch (Exception e) {
-            logger.error("getNativeAssetBalance error...", e);
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    public JSONObject queryOep5Info(String contractHash) {
-        try {
-            OntSdk ontSdk = getOep5OntSdk(contractHash);
-            String name = ontSdk.neovm().oep5().queryName();
-            String symbol = ontSdk.neovm().oep5().querySymbol();
-            String total = ontSdk.neovm().oep5().queryTotalSupply();
-
-            JSONObject oep5Obj = new JSONObject();
-            oep5Obj.put("Name", name);
-            oep5Obj.put("Symbol", symbol);
-            oep5Obj.put("TotalSupply", total);
-
-            return oep5Obj;
-        } catch (Exception e) {
-            logger.error("getNativeAssetBalance error...", e);
-            e.printStackTrace();
-            return null;
-        }
-    }
 
     private OntSdk getOntSdk() {
         OntSdk wm = OntSdk.getInstance();
-        wm.setRestful(configParam.MASTERNODE_RESTFUL_URL);
+        wm.setRestful(paramsConfig.MASTERNODE_RESTFUL_URL);
         return wm;
     }
 
-    private OntSdk getOep8OntSdk(String codeHash) throws Exception {
+    private OntSdk getOep8OntSdk(String codeHash) {
         OntSdk wm = OntSdk.getInstance();
-        wm.setRestful(configParam.MASTERNODE_RESTFUL_URL);
+        wm.setRestful(paramsConfig.MASTERNODE_RESTFUL_URL);
         wm.neovm().oep8().setContractAddress(codeHash);
         return wm;
     }
 
     private OntSdk getOep4OntSdk(String codeHash) {
         OntSdk wm = OntSdk.getInstance();
-        wm.setRestful(configParam.MASTERNODE_RESTFUL_URL);
+        wm.setRestful(paramsConfig.MASTERNODE_RESTFUL_URL);
         wm.neovm().oep4().setContractAddress(codeHash);
         return wm;
     }
 
     private OntSdk getOep5OntSdk(String codeHash) {
         OntSdk wm = OntSdk.getInstance();
-        wm.setRestful(configParam.MASTERNODE_RESTFUL_URL);
+        wm.setRestful(paramsConfig.MASTERNODE_RESTFUL_URL);
         wm.neovm().oep5().setContractAddress(codeHash);
         return wm;
     }
 
-    public JSONObject queryOep8Info(String contractHash, String[] tokenIds) {
-        try {
-            OntSdk ontSdk = getOep8OntSdk(contractHash);
-
-            String[] names = new String[tokenIds.length];
-            String[] symbols = new String[tokenIds.length];
-            String[] totalSupplys = new String[tokenIds.length];
-            for (int i = 0; i < tokenIds.length; i++) {
-                names[i] = ontSdk.neovm().oep8().queryName(Helper.hexToBytes(tokenIds[i]));
-                symbols[i] = ontSdk.neovm().oep8().querySymbol(Helper.hexToBytes(tokenIds[i]));
-                totalSupplys[i] = ontSdk.neovm().oep8().queryTotalSupply(Helper.hexToBytes(tokenIds[i]));
-            }
-
-            JSONObject oep4Obj = new JSONObject();
-            oep4Obj.put("Name", names);
-            oep4Obj.put("Symbol", symbols);
-            oep4Obj.put("TotalSupply", totalSupplys);
-            return oep4Obj;
-        } catch (Exception e) {
-            logger.error("getNativeAssetBalance error...", e);
-            e.printStackTrace();
-            return null;
-        }
-    }
 }
