3b942e8ac7249b7866d495d4e270be9344ad8684
zzsZhou <18217008370@163.com>
2019-05-15 11:34:04 +0800

    格式化

--- back-end-projects/Explorer/sql/tbl_oep5_dragon.sql
+++ back-end-projects/Explorer/sql/tbl_oep5_dragon.sql
@@ -16,10 +16,12 @@ SET FOREIGN_KEY_CHECKS=0;
 -- Table structure for tbl_ont_oep5_dragon
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_oep5_dragon`;
-CREATE TABLE `tbl_oep5_dragon` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `contract_hash` varchar(255) NOT NULL DEFAULT '' COMMENT '合约hash',
-  `asset_name` varchar(255) NOT NULL DEFAULT '' COMMENT '该dragon的名称',
-  `json_url` varchar(255) NOT NULL COMMENT '该dragon的基本信息url',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `tbl_oep5_dragon`
+(
+    `id`            int(11)      NOT NULL AUTO_INCREMENT,
+    `contract_hash` varchar(255) NOT NULL DEFAULT '' COMMENT '合约hash',
+    `asset_name`    varchar(255) NOT NULL DEFAULT '' COMMENT '该dragon的名称',
+    `json_url`      varchar(255) NOT NULL COMMENT '该dragon的基本信息url',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
--- back-end-projects/Explorer/sql/tbl_tx_eventlog.sql
+++ back-end-projects/Explorer/sql/tbl_tx_eventlog.sql
@@ -16,15 +16,17 @@ SET FOREIGN_KEY_CHECKS=0;
 -- Table structure for tbl_ont_block
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_tx_eventlog`;
-CREATE TABLE `tbl_tx_eventlog` (
-  `tx_hash` varchar(64) NOT NULL DEFAULT '' COMMENT '交易hash值',
-  `tx_type` int(11) NOT NULL COMMENT '区块链交易类型，208：部署合约交易 209：调用合约交易',
-  `tx_time` int(11) NOT NULL COMMENT '交易时间戳',
-  `block_height` int(11) NOT NULL COMMENT '区块高度',
-  `block_index` int(11) NOT NULL COMMENT '交易在区块里的索引',
-  `event_log` text NOT NULL COMMENT '交易的event log',
-  `called_contract_hash` varchar(255) NOT NULL DEFAULT '' COMMENT '该交易真正调用的合约hash',
-  PRIMARY KEY (`tx_hash`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `tbl_tx_eventlog`
+(
+    `tx_hash`              varchar(64)  NOT NULL DEFAULT '' COMMENT '交易hash值',
+    `tx_type`              int(11)      NOT NULL COMMENT '区块链交易类型，208：部署合约交易 209：调用合约交易',
+    `tx_time`              int(11)      NOT NULL COMMENT '交易时间戳',
+    `block_height`         int(11)      NOT NULL COMMENT '区块高度',
+    `block_index`          int(11)      NOT NULL COMMENT '交易在区块里的索引',
+    `event_log`            varchar(5000)NOT NULL COMMENT '交易的event log',
+    `called_contract_hash` varchar(255) NOT NULL DEFAULT '' COMMENT '该交易真正调用的合约hash',
+    PRIMARY KEY (`tx_hash`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 
