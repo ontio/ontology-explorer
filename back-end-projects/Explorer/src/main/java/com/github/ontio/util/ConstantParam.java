75005b95c8757015ac287864726af988b72ac7ed
zzsZhou <18217008370@163.com>
2019-05-22 16:00:14 +0800

    mapper分成三类设置不同的redis过期时间

--- back-end-projects/Explorer/src/main/java/com/github/ontio/util/ConstantParam.java
+++ back-end-projects/Explorer/src/main/java/com/github/ontio/util/ConstantParam.java
@@ -103,7 +103,7 @@ public class ConstantParam {
 
 
     /**
-     * 对外访问实时性要求不高的接口中对应的mapper，redis中可以设置过期时间较长
+     * 对外访问实时性要求不高的接口中对应的mapper，属于历史数据和统计数据，基本不会修改。redis中可以设置过期时间较长，
      */
     public static final List<String> REDIS_LONGEXPIRETIME_KEYLIST = new ArrayList<String>(
             Arrays.asList(
@@ -111,17 +111,27 @@ public class ConstantParam {
                     "BlockMapper.selectOneByHash",
                     "TxDetailMapper.selectTxByHash",
                     "TxDetailMapper.selectTransferTxDetailByHash",
-                    "TxDetailMapper.selectLatestOntTransferTxTime",
                     "TxDetailMapper.selectTxsByBlockHeight",
-                    "ContractMapper.selectApprovedContract",
-                    "ContractMapper.selectApprovedContractCount",
                     "ContractMapper.selectContractDetail",
-                    "Oep4Mapper.selectOep4Tokens",
-                    "Oep5Mapper.selectOep5Tokens",
-                    "Oep8Mapper.selectOep8Tokens",
                     "ContractDailySummaryMapper.selectDailySummaryByContractHash",
                     "DailySummaryMapper.selectSummaryByTime",
                     "DailySummaryMapper.selectAddrAndOntIdTotal"
+            )
+    );
+
+
+    /**
+     * 对外访问实时性要求不高的接口中对应的mapper，数据变动频率小，redis中可以设置过期时间较长
+     */
+    public static final List<String> REDIS_MEDIUMEXPIRETIME_KEYLIST = new ArrayList<String>(
+            Arrays.asList(
+                    "ContractMapper.selectApprovedContract",
+                    "ContractMapper.selectApprovedContractCount",
+                    "Oep4Mapper.selectOep4Tokens",
+                    "Oep5Mapper.selectOep5Tokens",
+                    "Oep8Mapper.selectOep8Tokens",
+                    "Oep8Mapper.selectAuditPassedOep8",
+                    "TxEventLogMapper.queryTxCount"
             )
     );
 
