c1b0b1c952e87657bebe9a2dc2a6eab35d1ad3bd
zzsZhou <18217008370@163.com>
2019-05-27 17:40:30 +0800

    add

---
+++ back-end-projects/Explorer/src/main/java/com/github/ontio/model/dto/TxEventLogDto.java
@@ -0,0 +1,18 @@
+package com.github.ontio.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.github.ontio.model.dao.TxEventLog;
+import lombok.Data;
+
+import javax.persistence.Table;
+
+/**
+ * @author zhouq
+ * @version 1.0
+ * @date 2019/5/26
+ */
+@Data
+@Table(name = "tbl_tx_eventlog")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class TxEventLogDto extends TxEventLog {
+}
