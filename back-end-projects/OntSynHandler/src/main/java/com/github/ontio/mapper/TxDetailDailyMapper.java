f684661dfb62c8d83a115bb150e539f988a9b63f
zzsZhou <18217008370@163.com>
2019-05-27 10:35:19 +0800

    删除无用sql

--- back-end-projects/OntSynHandler/src/main/java/com/github/ontio/mapper/TxDetailDailyMapper.java
+++ back-end-projects/OntSynHandler/src/main/java/com/github/ontio/mapper/TxDetailDailyMapper.java
@@ -1,7 +1,6 @@
 package com.github.ontio.mapper;
 
 import com.github.ontio.model.dao.TxDetailDaily;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
 import tk.mybatis.mapper.common.Mapper;
 
@@ -10,11 +9,6 @@ import java.util.List;
 @Component
 public interface TxDetailDailyMapper extends Mapper<TxDetailDaily> {
 
-    void deleteByHeight(@Param("blockHeight") int height);
-
-    int selectCountByHeight(@Param("blockHeight") int height);
-
     void batchInsert(List<TxDetailDaily> list);
-
 
 }
