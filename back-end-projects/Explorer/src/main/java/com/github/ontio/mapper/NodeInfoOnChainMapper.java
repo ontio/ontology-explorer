c427b73f531b6e716e94e4f90d9836803a1e5e12
dingxin <wdx7266@vip.qq.com>
2019-06-26 14:29:05 +0800

    add table for nodes info

---
+++ back-end-projects/Explorer/src/main/java/com/github/ontio/mapper/NodeInfoOnChainMapper.java
@@ -0,0 +1,7 @@
+package com.github.ontio.mapper;
+
+import com.github.ontio.model.dao.NodeInfoOnChain;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface NodeInfoOnChainMapper extends Mapper<NodeInfoOnChain> {
+}
