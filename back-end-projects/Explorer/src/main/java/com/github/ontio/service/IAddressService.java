a46a476a5d5aa3b572f5367af0f7102d0c8eef63
zzsZhou <18217008370@163.com>
2019-07-10 14:27:39 +0800

    get balance use new batch query api

--- back-end-projects/Explorer/src/main/java/com/github/ontio/service/IAddressService.java
+++ back-end-projects/Explorer/src/main/java/com/github/ontio/service/IAddressService.java
@@ -11,6 +11,8 @@ public interface IAddressService {
 
     ResponseBean queryAddressBalance(String address, String tokenType);
 
+    ResponseBean queryAddressBalanceByAssetName(String address, String assetName);
+
     ResponseBean queryTransferTxsByPage(String address, String assetName, Integer pageNumber, Integer pageSize);
 
     ResponseBean queryTransferTxsByTime(String address, String assetName, Long beginTime, Long endTime);
