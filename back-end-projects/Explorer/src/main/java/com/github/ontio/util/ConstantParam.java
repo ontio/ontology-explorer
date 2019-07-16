501981dd73f19af1763244e5e5701f9073ae8b43
zzsZhou <18217008370@163.com>
2019-07-16 16:46:01 +0800

    update 'TxDetailMapper.selectTxByHash' expires
    Former-commit-id: 96fd263a32854014c77dda9b908e806f2135002c

--- back-end-projects/Explorer/src/main/java/com/github/ontio/util/ConstantParam.java
+++ back-end-projects/Explorer/src/main/java/com/github/ontio/util/ConstantParam.java
@@ -113,7 +113,6 @@ public class ConstantParam {
             Arrays.asList(
                     "BlockMapper.selectOneByHeight",
                     "BlockMapper.selectOneByHash",
-                    "TxDetailMapper.selectTxByHash",
                     "TxDetailMapper.selectTransferTxDetailByHash",
                     "TxEventLogMapper.selectTxsByBlockHeight",
                     "ContractMapper.selectContractDetail",
@@ -130,6 +129,7 @@ public class ConstantParam {
      */
     public static final List<String> REDIS_MEDIUMEXPIRETIME_KEYLIST = new ArrayList<String>(
             Arrays.asList(
+                    "TxDetailMapper.selectTxByHash",
                     "ContractMapper.selectApprovedContract",
                     "ContractMapper.selectApprovedContractCount",
                     "Oep4Mapper.selectOep4Tokens",
