720684eee4a07ced8216487135134433ed742305
NashMiao <wdx7266@vip.qq.com>
2019-08-12 15:43:59 +0800

    update tbl and define dto

--- back-end-projects/Explorer/sql/tbl_net_node_info.sql
+++ back-end-projects/Explorer/sql/tbl_net_node_info.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS `tbl_net_node_info`
     ip               VARCHAR(15) NOT NULL,
     version          VARCHAR(50) NOT NULL DEFAULT '',
     is_consensus     BOOLEAN     NOT NULL,
-    active           BOOLEAN     NOT NULL,
+    is_active        BOOLEAN     NOT NULL,
     last_active_time BIGINT      NOT NULL,
     country          VARCHAR(20) NOT NULL DEFAULT '',
     longitude        VARCHAR(20) NOT NULL DEFAULT '',
--- back-end-projects/Explorer/src/main/java/com/github/ontio/model/dao/NetNodeInfo.java
+++ back-end-projects/Explorer/src/main/java/com/github/ontio/model/dao/NetNodeInfo.java
@@ -1,7 +1,14 @@
 package com.github.ontio.model.dao;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "tbl_net_node_info")
 public class NetNodeInfo {
     @Id
@@ -13,7 +20,8 @@ public class NetNodeInfo {
     @Column(name = "is_consensus")
     private Boolean isConsensus;
 
-    private Boolean active;
+    @Column(name = "is_active")
+    private Boolean isActive;
 
     @Column(name = "last_active_time")
     private Long lastActiveTime;
@@ -24,115 +32,4 @@ public class NetNodeInfo {
 
     private String latitude;
 
-    /**
-     * @return ip
-     */
-    public String getIp() {
-        return ip;
-    }
-
-    /**
-     * @param ip
-     */
-    public void setIp(String ip) {
-        this.ip = ip == null ? null : ip.trim();
-    }
-
-    /**
-     * @return version
-     */
-    public String getVersion() {
-        return version;
-    }
-
-    /**
-     * @param version
-     */
-    public void setVersion(String version) {
-        this.version = version == null ? null : version.trim();
-    }
-
-    /**
-     * @return is_consensus
-     */
-    public Boolean getIsConsensus() {
-        return isConsensus;
-    }
-
-    /**
-     * @param isConsensus
-     */
-    public void setIsConsensus(Boolean isConsensus) {
-        this.isConsensus = isConsensus;
-    }
-
-    /**
-     * @return active
-     */
-    public Boolean getActive() {
-        return active;
-    }
-
-    /**
-     * @param active
-     */
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
-
-    /**
-     * @return last_active_time
-     */
-    public Long getLastActiveTime() {
-        return lastActiveTime;
-    }
-
-    /**
-     * @param lastActiveTime
-     */
-    public void setLastActiveTime(Long lastActiveTime) {
-        this.lastActiveTime = lastActiveTime;
-    }
-
-    /**
-     * @return country
-     */
-    public String getCountry() {
-        return country;
-    }
-
-    /**
-     * @param country
-     */
-    public void setCountry(String country) {
-        this.country = country == null ? null : country.trim();
-    }
-
-    /**
-     * @return longitude
-     */
-    public String getLongitude() {
-        return longitude;
-    }
-
-    /**
-     * @param longitude
-     */
-    public void setLongitude(String longitude) {
-        this.longitude = longitude == null ? null : longitude.trim();
-    }
-
-    /**
-     * @return latitude
-     */
-    public String getLatitude() {
-        return latitude;
-    }
-
-    /**
-     * @param latitude
-     */
-    public void setLatitude(String latitude) {
-        this.latitude = latitude == null ? null : latitude.trim();
-    }
 }
