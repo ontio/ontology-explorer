fd164b9991adc94abee5bd987040283770e0a73c
leej1012 <jlee921012@163.com>
2023-07-03 15:56:27 +0800

    Update NodeInspire

--- back-end-projects/Explorer/src/main/java/com/github/ontio/model/dao/NodeInspire.java
+++ back-end-projects/Explorer/src/main/java/com/github/ontio/model/dao/NodeInspire.java
@@ -30,6 +30,12 @@ public class NodeInspire {
     @Column(name = "current_stake")
     private Long currentStake;
 
+    @Column(name = "init_pos")
+    private Long initPos;
+
+    @Column(name = "total_pos")
+    private Long totalPos;
+
     @Column(name = "node_released_ong_incentive")
     private Long nodeReleasedOngIncentive;
 
