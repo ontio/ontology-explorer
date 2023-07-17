9601f2dc63e804ab4b149152693a4cd155fa53fd
leej1012 <jlee921012@163.com>
2023-07-17 14:34:28 +0800

    Update tbl_contract column token_sum

--- back-end-projects/Explorer/src/main/resources/db/migration/V1.78__tbl_node_inspire.sql
+++ back-end-projects/Explorer/src/main/resources/db/migration/V1.78__tbl_node_inspire.sql
@@ -1,2 +1,3 @@
 ALTER TABLE tbl_node_inspire ADD COLUMN `node_apy` varchar(100) DEFAULT NULL COMMENT '节点年化收益';
 ALTER TABLE tbl_node_inspire ADD COLUMN `user_apy` varchar(100) DEFAULT NULL COMMENT '用户年化收益';
+ALTER TABLE tbl_contract MODIFY COLUMN `token_sum` VARCHAR(5000) NOT NULL COMMENT '该合约的总的token流通量.json格式字符串';
